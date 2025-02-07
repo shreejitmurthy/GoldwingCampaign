abb058071276b7a211e8eab5c05e307d054edc45
Shreejit Murthy <botmorph17@gmail.com>
2025-02-07 17:51:16 +1030

    overloads for asset object arguments instead of paths

--- src/Include/AssetManager.hpp
+++ src/Include/AssetManager.hpp
@@ -12,17 +12,14 @@ private:
 
 public:
     void LoadFontAsset(const std::string& name, const std::string& filePath);
-
-    void LoadFontAsset(const std::string& name, const std::string& filePath, int fontSize);
-
+    void LoadFontAsset(const std::string& name, Font font);
     void LoadTextureAsset(const std::string& name, const std::string& filePath);
-
+    void LoadTextureAsset(const std::string& name, Texture texture);
     void LoadModelAsset(const std::string& name, const std::string& filePath);
+    void LoadModelAsset(const std::string& name, Model model);
 
     Font GetFont(const std::string& name);
-
     Texture2D GetTexture(const std::string& name);
-
     Model GetModel(const std::string& name);
 
     void UnloadAll();
